3ba1dca8a45b5020e49ded85e3a5fc5fb8ea4687
PriyaBaiM <46711345+PriyaBaiM@users.noreply.github.com>
2025-01-17 04:30:02 +0000

    Added filter in monthly rewards

--- src/components/MonthlyRewardsTable.js
+++ src/components/MonthlyRewardsTable.js
@@ -1,33 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { getMonthName } from '../utils/DateUtils';
 
-const MonthlyRewardsTable = ({ monthlyRewards }) => (
-  <div>
-    <h2>User Monthly Rewards</h2>
-    <table>
-      <thead>
-        <tr>
-          <th>Customer ID</th>
-          <th>Name</th>
-          <th>Month</th>
-          <th>Year</th>
-          <th>Reward Points</th>
-        </tr>
-      </thead>
-      <tbody>
-        {monthlyRewards.map((reward, index) => (
-          <tr key={index}>
-            <td>{reward.customerId}</td>
-            <td>{reward.name}</td>
-            <td>{reward.month}</td>
-            <td>{reward.year}</td>
-            <td>{reward.points}</td>
+const MonthlyRewardsTable = ({ monthlyRewards }) => {
+  const [selectedMonth, setSelectedMonth] = useState('');
+
+  const handleMonthChange = (event) => {
+    setSelectedMonth(event.target.value);
+  };
+
+  const filteredRewards = selectedMonth
+    ? monthlyRewards.filter(reward => getMonthName(reward.month) === selectedMonth)
+    : monthlyRewards;
+
+  const uniqueMonths = [...new Set(monthlyRewards.map(reward => getMonthName(reward.month)))];
+
+  return (
+    <div>
+      <h2>User Monthly Rewards</h2>
+      <div className="filter-container">
+        <label htmlFor="monthFilter">Filter by Month: </label>
+        <select id="monthFilter" value={selectedMonth} onChange={handleMonthChange}>
+          <option value="">All</option>
+          {uniqueMonths.map((month, index) => (
+            <option key={index} value={month}>{month}</option>
+          ))}
+        </select>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Customer ID</th>
+            <th>Name</th>
+            <th>Month</th>
+            <th>Year</th>
+            <th>Reward Points</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
-);
+        </thead>
+        <tbody>
+          {filteredRewards.map((reward, index) => (
+            <tr key={index}>
+              <td>{reward.customerId}</td>
+              <td>{reward.name}</td>
+              <td>{getMonthName(reward.month)}</td>
+              <td>{reward.year}</td>
+              <td>{reward.points}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 MonthlyRewardsTable.propTypes = {
   monthlyRewards: PropTypes.arrayOf(PropTypes.object).isRequired,
